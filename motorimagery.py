c8ac821b26f43d3c18026e33344208fa20f3fa80
jnauly <jnaulty@gmail.com>
2014-07-25 13:00:14 -0700

    working motorimagery test

--- motorimagery.py
+++ motorimagery.py
@@ -24,6 +24,16 @@ class MotorImagery(object):
         self.numtrials = numtrials
         self.waitdur = waitdur
         self.port = port
+        self.text1='Right-Hand'
+        self.text2='Left-Hand'
+        self.text3='Feet'
+        self.text4='Tongue'
+        self.pattern1 = visual.TextStim(win=self.mywin, text=self.text1)
+        self.pattern2 = visual.TextStim(win=self.mywin, text=self.text2)
+        self.pattern3 = visual.TextStim(win=self.mywin, text=self.text3)
+        self.pattern4 = visual.TextStim(win=self.mywin, text=self.text4)
+
+        
 
     def collecting(self):
         self.collector = csv_collector.CSVCollector(fname=self.fname, port= self.port)
@@ -35,47 +45,8 @@ class MotorImagery(object):
    
     def start(self):
         
-        self.text1='Right-Hand'
-        self.text2='Left-Hand'
-        self.text3='Feet'
-        self.text4='Tongue'
-        self.pattern1 = visual.TextStim(win=self.mywin, text=self.text1, font='', pos=(0.0, 0.0), depth=0, rgb=None, color=(1.0, 1.0, 1.0),
-         colorSpace='rgb', opacity=1.0,contrast=1.0, units='', ori=0.0,
-          height=None, antialias=True, bold=False, italic=False, alignHoriz='center',
-          alignVert='center', fontFiles=[],
-         wrapWidth=None, flipHoriz=False, flipVert=False, name=None, autoLog=None)
-        self.pattern2 = visual.TextStim(win=self.mywin, text=self.text1, font='', pos=(0.0, 0.0), depth=0, rgb=None, color=(1.0, 1.0, 1.0),
-         colorSpace='rgb', opacity=1.0,contrast=1.0, units='', ori=0.0,
-          height=None, antialias=True, bold=False, italic=False, alignHoriz='center',
-          alignVert='center', fontFiles=[],
-         wrapWidth=None, flipHoriz=False, flipVert=False, name=None, autoLog=None)
-        self.pattern3 = visual.TextStim(win=self.mywin, text=self.text1, font='', pos=(0.0, 0.0), depth=0, rgb=None, color=(1.0, 1.0, 1.0),
-         colorSpace='rgb', opacity=1.0,contrast=1.0, units='', ori=0.0,
-          height=None, antialias=True, bold=False, italic=False, alignHoriz='center',
-          alignVert='center', fontFiles=[],
-         wrapWidth=None, flipHoriz=False, flipVert=False, name=None, autoLog=None)
-        self.pattern4 = visual.TextStim(win=self.mywin, text=self.text1, font='', pos=(0.0, 0.0), depth=0, rgb=None, color=(1.0, 1.0, 1.0),
-         colorSpace='rgb', opacity=1.0,contrast=1.0, units='', ori=0.0,
-          height=None, antialias=True, bold=False, italic=False, alignHoriz='center',
-          alignVert='center', fontFiles=[],
-         wrapWidth=None, flipHoriz=False, flipVert=False, name=None, autoLog=None)
-        """
-        self.pattern2 = visual.TextStim(win=self.mywin, name=self.text2, units='cm', 
-                        tex=None, mask=None,
-                        ori=0, pos=[0, 0], size=10, sf=1, phase=0,
-                        color=[-1,-1,-1], colorSpace='rgb', opacity=1,
-                        texRes=256, interpolate=True, depth=-2.0)
-        self.pattern3 = visual.TextStim(win=self.mywin, text=self.text3, name='pattern1',units='cm', 
-                        tex=None, mask=None,
-                        ori=0, pos=[0, 0], size=10, sf=1, phase=0.0,
-                        color=[1,1,1], colorSpace='rgb', opacity=1, 
-                        texRes=256, interpolate=True, depth=-1.0)
-        self.pattern4 = visual.TextStim(win=self.mywin, name=self.text4 ,units='cm', 
-                        tex=None, mask=None,
-                        ori=0, pos=[0, 0], size=10, sf=1, phase=0,
-                        color=[-1,-1,-1], colorSpace='rgb', opacity=1,
-                        texRes=256, interpolate=True, depth=-2.0)
-        """
+        
+        
         ###Testing framerate grabber###
         print self.mywin.getActualFrameRate()
         self.Trialclock = core.Clock()
@@ -101,14 +72,19 @@ class MotorImagery(object):
 
                 #draws square and fixation on screen.
                 if self.count == 0:
-                    self.pattern1.setAutoDraw(True)
+                    self.pattern1.draw()
+                    self.mywin.flip()
+                
                 elif self.count == 1:
-                    self.pattern2.setAutoDraw(True)
+                    self.pattern2.draw()
+                    self.mywin.flip()
                 elif self.count == 2:
-                    self.pattern3.setAutoDraw(True)
+                    self.pattern3.draw()
+                    self.mywin.flip()
                 elif self.count == 3:
-                    self.pattern4.setAutoDraw(True)
-
+                    self.pattern4.draw()
+                    self.mywin.flip()
+                    
                 """         
                 ###Tagging the data with the calculated frequency###
                 Attempting to only get 1 sample tagged, however, this is hard.
@@ -148,9 +124,10 @@ Just run this program by itself if you don't want to use run.py
 
 """
 
-if "__name__" == "__main__":
-    motorimagery = MotorImagery()
-    motorimagery.start()
+motorimagery = MotorImagery()
+
+motorimagery.start()
+
 
 
    
